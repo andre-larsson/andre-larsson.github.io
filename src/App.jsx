d3ae0fb157b52222f908ab1055194aa6da2bcb8b
André Larsson <larsson.andre@gmail.com>
2026-02-11 16:25:06 +0100

    Initial Vite React site

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="page">
+      <h1>Empty page</h1>
+      <p>Nothing here yet.</p>
+    </main>
+  )
+}
+
+export default App
