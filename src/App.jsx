75c38c066b511b955c7af1ec1afc3ae8be86f746
André Larsson <larsson.andre@gmail.com>
2026-03-10 00:58:34 +0100

    Add Quiz System link to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,10 @@ function App() {
           <a href="https://andre-larsson.github.io/interval-trainer/">Interval Trainer</a>
           <span>Öva gehör med intervallträning direkt i webbläsaren (på svenska 🇸🇪).</span>
         </div>
+        <div className="link-item">
+          <a href="https://andre-larsson.github.io/quiz-system/">Quiz System</a>
+          <span>Modulärt quiz med frågor i enkla JSON-filer (på svenska 🇸🇪).</span>
+        </div>
       </section>
     </main>
   )
